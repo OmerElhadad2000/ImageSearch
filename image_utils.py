9bd374bc68a9ce07ac7d489f31e949e1d40f7473
omerelhadad2000 <omer.elh11@gmail.com>
2025-08-23 21:39:38 +0300

    init

---
+++ image_utils.py
@@ -0,0 +1,14 @@
+from itertools import chain
+from pathlib import Path
+
+
+def display_image(image):
+    """Display a single image."""
+    image.show()
+
+
+def get_images(path: str) -> list[str]:
+    p = Path(path).expanduser()
+    uris = [str(f)[2::] for f in chain(p.glob("*.jpeg"), p.glob("*.png"), p.glob("*.jpg")) if f.is_file()]
+
+    return uris
